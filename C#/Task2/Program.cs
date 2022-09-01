130a000d820b7236d82c79af436868a7636bda10
VictoriyaS <vika.s.v.o@mail.ru>
2022-09-01 16:28:13 +1000

    Task2

---
+++ C#/Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("a=");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.Write("b=");
+int num2=Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+Console.Write("max=");
+Console.Write(max);
