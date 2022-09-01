3eac9dd106b9e7491f60afccefa4a7cb1729c136
VictoriyaS <vika.s.v.o@mail.ru>
2022-09-01 17:16:03 +1000

    Task6

---
+++ C#/Task6/Program6.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
