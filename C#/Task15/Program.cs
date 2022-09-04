85282a688a93add74d1bf8325f95ee4c9c9f5aa4
VictoriyaS <vika.s.v.o@mail.ru>
2022-09-04 13:46:18 +1000

    Task15

---
+++ C#/Task15/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите порядковый номер дня недели");
+int num = Convert.ToInt32(Console.ReadLine());
+if ( num == 1 || num == 2 || num == 3 || num == 4 || num == 5)
+Console.WriteLine("нет");
+if ( num == 6 || num == 7)
+Console.WriteLine("да");
