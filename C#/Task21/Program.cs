fdcd2c4ba3c0998cc7a19973e4aa51dbefa681c5
VictoriyaS <vika.s.v.o@mail.ru>
2022-09-09 14:25:47 +1000

    Task21

---
+++ C#/Task21/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите координаты точки А: ");
+int ax = Convert.ToInt32(Console.ReadLine());
+int ay = Convert.ToInt32(Console.ReadLine());
+int az = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B: ");
+int bx = Convert.ToInt32(Console.ReadLine());
+int by = Convert.ToInt32(Console.ReadLine());
+int bz = Convert.ToInt32(Console.ReadLine());
+
+double AB = Math.Sqrt(((bx - ax)*(bx - ax) + (by - ay)*(by - ay) + (bz - az)*(bz - az)));
+Console.WriteLine(AB);
