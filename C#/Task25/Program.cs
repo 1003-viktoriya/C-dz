d9e2f0ced1df4c43d786d2f9f97969d713f8ae84
VictoriyaS <vika.s.v.o@mail.ru>
2022-09-12 15:01:52 +1000

    Task25

---
+++ C#/Task25/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число A: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{Math.Pow(num, num1)} ");
