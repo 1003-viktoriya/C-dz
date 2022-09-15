059988019cc107804d90bbf54e03109e22e84b8f
VictoriyaS <vika.s.v.o@mail.ru>
2022-09-15 15:28:45 +1000

    Ex38

---
+++ C#/Task38/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива
+int [] array = new int [4];
+void NewArray (int[]array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 80);
+    }
+}
+NewArray(array);
+void PrintArray(int[]array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write($"  {array[i]}");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
+double max = 0;
+double min = 0;
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i] > max)
+    max = array[i];
+    if (array[i] < min)
+    min = array[i];
+}
+double diff = max - min;
+Console.WriteLine($"Разница между max и min числами = " + (diff));
+
