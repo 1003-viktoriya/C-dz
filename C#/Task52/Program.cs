31bb18a66879de6b3c0a69ab26637866d02530c1
VictoriyaS <vika.s.v.o@mail.ru>
2022-09-23 23:28:42 +1000

    Task52

---
+++ C#/Task52/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце
+Console.WriteLine("Введите размерность массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] arr = new int[m,n];
+for (int i=0; i<m; i++)
+{
+    for (int j=0; j<n; j++)
+    {
+        arr[i,j] = new Random().Next(0,21);
+        Console.Write($"{arr[i,j]}  ");
+    }
+    Console.WriteLine();
+}
+for (int j=0; j<n; j++)
+{
+    double sum=0;
+    for (int i=0; i<m; i++)
+    {
+        sum +=arr[i,j];
+    }
+    double avrg = sum/m;
+    
+Console.Write($"{Math.Round(avrg, 2)}  ");
+}
