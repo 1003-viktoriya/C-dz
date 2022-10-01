9fa773e6871af1f5f59052b56c6cb2d1b52bb1c8
VictoriyaS <vika.s.v.o@mail.ru>
2022-10-01 23:11:58 +1000

    Task64

---
+++ C#/Task64/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1
+void PrintNumbers(int num)
+{
+    if (num==0) return;
+    {
+        Console.Write($"{num} ");
+        PrintNumbers(num-1);
+        
+    }
+}
+PrintNumbers(10);
